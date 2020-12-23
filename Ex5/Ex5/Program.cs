d1c049d1b065580786cd2dc7928c7174e1d35bae
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-23 12:43:42 +0100

    ex5

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int a = 1, b = 2, c = 3, d = 4;
+            b = c;
+            c = a;
+            a = d;
+            d = b;
+        }
+    }
+}
