3800cfad24bb842a2744eaeebc01e9d33eb78108
Arnau Diez Sans <naudelsud@hotmail.com>
2020-12-23 12:40:11 +0100

    ex4

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int n = 0;
+
+            Console.WriteLine("Valor inicial de N:{0}",n);
+            n = n + 77;
+            Console.WriteLine("Valor de N + 77:{0}", n);
+            n = n - 3;
+            Console.WriteLine("Valor de N - 3:{0}", n);
+            n = n * 2;
+            Console.WriteLine("Valor de N * 2:{0}", n);
+        }
+    }
+}
